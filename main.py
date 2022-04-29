cb8698c0d592284461bb82c6464c8fc15263fe24
kdenz <kdenzo88@gmail.com>
2022-04-29 16:20:31 +0200

    debut main

---
+++ main.py
@@ -0,0 +1,41 @@
+from Perso.Personnage import Personnage
+
+
+def main():
+    print('Choix Personnage :')
+    PersoList = choix()
+    tour(PersoList)
+    return ;
+
+def choix():
+    value = input(" Personnage 1 (Mage =1 Guerrier =2 Healeur =3)")
+    if value == 1 :
+        Personnage1  = Personnage()
+        Personnage1  = Personnage.Mage()
+    if value == 2:
+        Personnage1  = Personnage()
+        Personnage1  = Personnage.Guerrier()
+    if value == 3:
+        Personnage1  = Personnage()
+        Personnage1  = Personnage.Healeur()
+    value = input(" Personnage 2 (Mage =1 Guerrier =2 Healeur =3)")
+    if value == 1 :
+        Personnage2  = Personnage()
+        Personnage2  = Personnage.Mage()
+    if value == 2:
+        Personnage2  = Personnage()
+        Personnage2  = Personnage.Guerrier()
+    if value == 3:
+        Personnage2  = Personnage()
+        Personnage2  = Personnage.Healeur()
+    Perso = []
+    Perso.append(Personnage1)
+    Perso.append(Personnage2)
+def tour(PersoList):
+     Perso1 = PersoList[0]
+     Perso2 = PersoList[1]
+     Win=False;
+     PersoWin =""
+     while Win == False:
+         return;
+
